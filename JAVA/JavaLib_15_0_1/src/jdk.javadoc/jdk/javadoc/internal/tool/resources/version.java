773567871c49de128a2b7ef6ab66723e27fbbd6d
안형빈 <nks0005@naver.com>
2021-11-05 04:51:51 +0900

    java 라이브러리 15.0.1 업로드

---
+++ JAVA/JavaLib_15_0_1/src/jdk.javadoc/jdk/javadoc/internal/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.javadoc.internal.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "15.0.1+9-18" },
+            { "jdk", "15.0.1" },
+            { "release", "15.0.1" },
+        };
+    }
+}
---
+++ JAVA/JavaLib_15_0_1/src/jdk.jdeps/com/sun/tools/javap/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javap.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "15.0.1+9-18" },
+            { "jdk", "15.0.1" },
+            { "release", "15.0.1" },
+        };
+    }
+}
